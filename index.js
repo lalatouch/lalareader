95986b69399725d72c4abd4729ebd103f572ce20
Clément Decoodt <clement@decoodt.eu>
2017-06-12 16:45:33 +0200

    Add empty base project

---
+++ index.js
@@ -0,0 +1,9 @@
+/* index.js - Main server entrypoint
+ *
+ * Copyright (C) 2017 LaLaTouch
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+console.log('Hello, world !');
