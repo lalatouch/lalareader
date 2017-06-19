5ccd462db01dd7497aa58d36c68c6cc1dff9cfd3
Clément Decoodt <clement@decoodt.eu>
2017-06-19 16:18:58 +0200

    Fix playlist reader prototype

--- common/models/playlist.js
+++ common/models/playlist.js
@@ -2,15 +2,16 @@
 
 module.exports = function(Playlist) {
 
-  Playlist.prototype.play = function() {
+  Playlist.prototype.play = function(cb) {
     console.log("Playing playlist " + this.id);
     this.tracks((err, tracks) => {
     });
+	cb(null);
   }
 
   Playlist.remoteMethod('prototype.play', {
-    accepts: {},
-    returns: {},
+    accepts: [],
+    returns: [],
     http: {path: '/play'}
   });
 
