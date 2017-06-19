e4e64e2eb6cce16e1545172fec882869159dc21b
Clément Decoodt <clement@decoodt.eu>
2017-06-19 16:21:55 +0200

    Add shuffle prototype

--- common/models/playlist.js
+++ common/models/playlist.js
@@ -4,6 +4,7 @@ module.exports = function(Playlist) {
 
   Playlist.prototype.play = function(cb) {
     console.log("Playing playlist " + this.id);
+	// TODO
     this.tracks((err, tracks) => {
     });
 	cb(null);
@@ -15,4 +16,17 @@ module.exports = function(Playlist) {
     http: {path: '/play'}
   });
 
+  Playlist.prototype.shuffle = function(cb) {
+    console.log("Shuffling playlist " + this.id);
+	// TODO
+    this.tracks((err, tracks) => {
+    });
+	cb(null);
+  }
+
+  Playlist.remoteMethod('prototype.shuffle', {
+    accepts: [],
+    returns: [],
+    http: {path: '/shuffle'}
+  });
 };
