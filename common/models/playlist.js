90c6bfcc3d99ea8e11b54439223735ae4cbd9d20
Clément Decoodt <clement@decoodt.eu>
2017-06-19 16:08:20 +0200

    Add playlist play function

--- common/models/playlist.js
+++ common/models/playlist.js
@@ -2,4 +2,16 @@
 
 module.exports = function(Playlist) {
 
+  Playlist.prototype.play = function() {
+    console.log("Playing playlist " + this.id);
+    this.tracks((err, tracks) => {
+    });
+  }
+
+  Playlist.remoteMethod('prototype.play', {
+    accepts: {},
+    returns: {},
+    http: {path: '/play'}
+  });
+
 };
