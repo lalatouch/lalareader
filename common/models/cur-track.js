764f6addda159f3f50612f101dd6066f30682e13
Clément Decoodt <clement@decoodt.eu>
2017-06-20 12:10:22 +0200

    Add logic keywords to API and TODOs

--- common/models/cur-track.js
+++ common/models/cur-track.js
@@ -3,8 +3,24 @@
 module.exports = function(Curtrack) {
 
   Curtrack.fastDir = function(dir, status, cb) {
-    console.log("Go forward, dir=" + dir + ", status=" + status);
-	// TODO
+    console.log("dir=" + dir + ", status=" + status);
+	if(dir === "forward") {
+		if(status === "go") {
+			// TODO Call player
+			console.log("Go fforward!");
+		} else if(status === "stop") {
+			// TODO Call player
+			console.log("Stop fforwarding...");
+		}
+	} else if(dir === "backward") {
+		if(status === "go") {
+			// TODO Call player
+			console.log("Go backward");
+		} else if(status === "stop") {
+			// TODO Call player
+			console.log("Stop going backward");
+		}
+	}
 	cb(null);
   }
 };
