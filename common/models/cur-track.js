4714176e25410a0c26ec32741c01b9c8704b93e6
Clément Decoodt <clement@decoodt.eu>
2017-06-20 12:05:29 +0200

    WIP: Fast forward/backward API definition

---
+++ common/models/cur-track.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function(Curtrack) {
+
+  Curtrack.fastDir = function(dir, status, cb) {
+    console.log("Go forward, dir=" + dir + ", status=" + status);
+	// TODO
+	cb(null);
+  }
+};
