da151164a21e3be52c65ae48f3201aef49694ab8
Clément Decoodt <clement@decoodt.eu>
2017-06-20 12:21:52 +0200

    Add error management

--- common/models/cur-track.js
+++ common/models/cur-track.js
@@ -3,6 +3,7 @@
 module.exports = function(Curtrack) {
 
   Curtrack.fastDir = function(dir, status, cb) {
+	var res;
     console.log("dir=" + dir + ", status=" + status);
 	if(dir === "forward") {
 		if(status === "go") {
@@ -11,6 +12,9 @@ module.exports = function(Curtrack) {
 		} else if(status === "stop") {
 			// TODO Call player
 			console.log("Stop fforwarding...");
+		} else {
+			res = new Error("Wrong status");
+			res.status = 400;
 		}
 	} else if(dir === "backward") {
 		if(status === "go") {
@@ -19,8 +23,33 @@ module.exports = function(Curtrack) {
 		} else if(status === "stop") {
 			// TODO Call player
 			console.log("Stop going backward");
+		} else {
+			res = new Error("Wrong status");
+			res.status = 400;
 		}
+	} else {
+		res = new Error("Wrong direction");
+		res.status = 400;
 	}
-	cb(null);
+	cb(res);
+  }
+
+  Curtrack.control = function(command, cb) {
+    console.log("command=" + command);
+	var res;
+	if(command === "play") {
+		//TODO Play
+		console.log("Playing music");
+	} else if(command === "pause") {
+		//TODO Pause
+		console.log("Pausing music");
+	} else if(command === "stop") {
+		//TODO Stop
+		console.log("Stopping music");
+	} else {
+		res = new Error("Command not recognized");
+		res.status = 400;
+	}
+	cb(res);
   }
 };
