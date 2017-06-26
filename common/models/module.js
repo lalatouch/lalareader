a11fa3aeb3479bb6fa50a8830e243760c219ab62
Tuetuopay <tuetuopay@me.com>
2017-06-26 12:29:35 +0200

    Added methods to control leds of modules

--- common/models/module.js
+++ common/models/module.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let http = require("http");
+
 module.exports = Module => {
 	Module.register = function(req, identifier, cb) {
 		// Find if it already exists
@@ -19,6 +21,20 @@ module.exports = Module => {
 
 	Module.prototype.playlist = function(cb) {
 		Module.app.models.Playlist.findById(this.playlistId, cb);
+	}
+
+	Module.prototype.lightUp = function(cb) {
+		http.get({
+			host: this.ip,
+			path: "/led?red=1024&green=1024&blue=1024"
+		}, cb);
+	}
+
+	Module.prototype.lightDown = function(cb) {
+		http.get({
+			host: this.ip,
+			path: "/led?red=0&green=0&blue=0"
+		}, cb);
 	}
 };
 
