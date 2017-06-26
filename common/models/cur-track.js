5522a542a05a1f8a36ff61c58755b7166dc24912
Tuetuopay <tuetuopay@me.com>
2017-06-26 05:39:44 +0200

    Reindent

--- common/models/cur-track.js
+++ common/models/cur-track.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const mpv = require('node-mpv');
- 
+
 var player = new mpv({
 	"audio_only": true
 });
@@ -16,91 +16,93 @@ var playerStat = { mute: false,
 };
 
 player.on('statuschange', function(status){
-  console.log(status);
-  playerStat = status;
+	console.log(status);
+	playerStat = status;
 });
- 
+
 module.exports = function(Curtrack) {
 
-  Curtrack.fastDir = function(dir, status, cb) {
-	var res;
-    console.log("dir=" + dir + ", status=" + status);
-	if(dir === "forward") {
-		if(status === "go") {
-			player.speed(10);
-			console.log("Go fforward!");
-		} else if(status === "stop") {
-			player.speed(1);
-			console.log("Stop fforwarding...");
-		} else {
-			res = new Error("Wrong status");
-			res.status = 400;
-		}
-	} else if(dir === "backward") {
-		if(status === "go") {
-			// TODO Go backward...
-			player.seek(-5);
-			console.log("Go backward");
-		} else if(status === "stop") {
-			player.speed(1);
-			console.log("Stop going backward");
-		} else {
-			res = new Error("Wrong status");
-			res.status = 400;
-		}
-	} else {
-		res = new Error("Wrong direction");
-		res.status = 400;
-	}
-	cb(res);
-  }
 
-  Curtrack.control = function(command, cb) {
-    console.log("command=" + command);
-	var res;
-	if(command === "play") {
-		// TODO Choose the current track
-		if(playerStat.pause == true && playerStat['playlist-count'] > 0)
-			player.togglePause();
-		else
-			player.loadFile("sample.mp3");
-		console.log("Playing music");
-	} else if(command === "pause") {
-		player.pause();
-		console.log("Pausing music");
-	} else if(command === "stop") {
-		player.stop();
-		console.log("Stopping music");
-	} else {
-		res = new Error("Command not recognized");
-		res.status = 400;
-	}
-	cb(res);
-  }
 
-  Curtrack.volume = function(pos, val, cb) {
-	console.log("pos=" + pos + ", val = " + val);
-	var res;
-	if(pos === "abs") {
-		if(val >= 0 && val <= 100) {
-			console.log("Set absolute volume to " + val);
-			player.volume(val);
+	Curtrack.fastDir = function(dir, status, cb) {
+		var res;
+		console.log("dir=" + dir + ", status=" + status);
+		if(dir === "forward") {
+			if(status === "go") {
+				player.speed(10);
+				console.log("Go fforward!");
+			} else if(status === "stop") {
+				player.speed(1);
+				console.log("Stop fforwarding...");
+			} else {
+				res = new Error("Wrong status");
+				res.status = 400;
+			}
+		} else if(dir === "backward") {
+			if(status === "go") {
+				// TODO Go backward...
+				player.seek(-5);
+				console.log("Go backward");
+			} else if(status === "stop") {
+				player.speed(1);
+				console.log("Stop going backward");
+			} else {
+				res = new Error("Wrong status");
+				res.status = 400;
+			}
 		} else {
-			res = new Error("Value out of bounds");
+			res = new Error("Wrong direction");
 			res.status = 400;
 		}
-	} else if(pos === "rel") {
-		if(val >= -100 && val <= 100) {
-			console.log("Adding " + val + " to current volume");
-			player.adjustVolume(val);
-		} else {
-			res = new Error("Value out of bounds");
-			res.status = 400;
-		}
-	} else {
-		res = new Error("Wrong positionning");
-		res.status = 400;
+		cb(res);
 	}
-	cb(res);
-  }
+
+	Curtrack.control = function(command, cb) {
+		console.log("command=" + command);
+		var res;
+		if(command === "play") {
+			// TODO Choose the current track
+			if(playerStat.pause == true && playerStat['playlist-count'] > 0)
+				player.togglePause();
+			else
+				player.loadFile("sample.mp3");
+			console.log("Playing music");
+		} else if(command === "pause") {
+			player.pause();
+			console.log("Pausing music");
+		} else if(command === "stop") {
+			player.stop();
+			console.log("Stopping music");
+		} else {
+			res = new Error("Command not recognized");
+			res.status = 400;
+		}
+		cb(res);
+	}
+
+	Curtrack.volume = function(pos, val, cb) {
+		console.log("pos=" + pos + ", val = " + val);
+		var res;
+		if(pos === "abs") {
+			if(val >= 0 && val <= 100) {
+				console.log("Set absolute volume to " + val);
+				player.volume(val);
+			} else {
+				res = new Error("Value out of bounds");
+				res.status = 400;
+			}
+		} else if(pos === "rel") {
+			if(val >= -100 && val <= 100) {
+				console.log("Adding " + val + " to current volume");
+				player.adjustVolume(val);
+			} else {
+				res = new Error("Value out of bounds");
+				res.status = 400;
+			}
+		} else {
+			res = new Error("Wrong positionning");
+			res.status = 400;
+		}
+		cb(res);
+	}
 };
--- common/models/playlist.js
+++ common/models/playlist.js
@@ -2,35 +2,38 @@
 
 module.exports = function(Playlist) {
 
-  Playlist.prototype.play = function(cb) {
-    console.log("Playing playlist " + this.id);
-	// TODO
-    this.tracks((err, tracks) => {
-    });
-	cb(null);
-  }
+	Playlist.prototype.play = function(cb) {
+		console.log("Playing playlist " + this.id);
 
-  Playlist.prototype.shuffle = function(cb) {
-    console.log("Shuffling playlist " + this.id);
-	// TODO
-    this.tracks((err, tracks) => {
-    });
-	cb(null);
-  }
+		// Keep track of the currently playing playlist
+		Playlist.nowPlaying = this;
+		this.tracks((err, tracks) => {
+			if (err) return cb(err);
+			else cb();
+		});
+	}
 
-  Playlist.prototype.next = function(cb) {
-    console.log("Playing next song");
-	// TODO
-    this.tracks((err, tracks) => {
-    });
-	cb(null);
-  }
+	Playlist.prototype.shuffle = function(cb) {
+		console.log("Shuffling playlist " + this.id);
+		// TODO
+		this.tracks((err, tracks) => {
+		});
+		cb(null);
+	}
 
-  Playlist.prototype.previous = function(cb) {
-    console.log("Playing previous song");
-	// TODO
-    this.tracks((err, tracks) => {
-    });
-	cb(null);
-  }
+	Playlist.prototype.next = function(cb) {
+		console.log("Playing next song");
+		// TODO
+		this.tracks((err, tracks) => {
+		});
+		cb(null);
+	}
+
+	Playlist.prototype.previous = function(cb) {
+		console.log("Playing previous song");
+		// TODO
+		this.tracks((err, tracks) => {
+		});
+		cb(null);
+	}
 };
